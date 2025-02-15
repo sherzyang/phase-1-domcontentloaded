e20240960a134fd20ad0cf1709dc732f1e52dc16
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-02-14 16:46:14 -0800

    Update.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+    let words = document.getElementById("text");
+    words.textContent = "This is really cool!"
+})
+
